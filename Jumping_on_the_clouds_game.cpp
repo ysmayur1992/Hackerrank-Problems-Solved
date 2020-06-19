cc9d7c569c6f23639818e98b424131e657780f63
Yash Mayur <66181778+ysmayur1992@users.noreply.github.com>
2020-06-19 09:44:52 -0400

    Update Jumping_on_the_clouds_game.cpp

--- Jumping_on_the_clouds_game.cpp
+++ Jumping_on_the_clouds_game.cpp
@@ -1,1 +1,19 @@
-
+int Jump(vector<int> c) {
+    int jump=0;
+    for(int i=0;i<c.size()-1;i=i){
+        if(c[i+1]==0 && c[i+2] == 0 && c[i]==0){
+            jump += 1;
+            i += 2;
+        } else if(c[i+1] != 0 && c[i+2] == 0 && c[i]==0){
+            jump += 1;
+            i+=2;
+        } else if(c[i+1] == 0 && c[i+2] != 0 && c[i]==0){
+            jump += 1;
+            i+=1;
+        } else if(c[i+1]==0 && c[i+2]==0 && c[i] != 0){
+            jump += 1;
+            i+=2;
+        } else jump = 0;
+    }
+    return jump;
+}
